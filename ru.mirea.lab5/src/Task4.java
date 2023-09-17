43fd7965d75b1144c63e46436ddad06bcb097302
Артём Чеботарёв <ponchikbom@gmail.com>
2023-09-17 22:17:25 +0300

    Lab5

---
+++ ru.mirea.lab5/src/Task4.java
@@ -0,0 +1,96 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Task4 extends JPanel implements Runnable {
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 400;
+    private static final int FRAME_RATE = 30; // Скорость анимации (кадры в секунду)
+
+    private BufferedImage[] frames; // Массив кадров
+    private int currentFrame = 0; // Индекс текущего кадра
+    private Thread animationThread;
+    private boolean running = false;
+
+    public Task4() {
+        frames = new BufferedImage[4]; // Пример: 4 кадра анимации
+
+        for (int i = 0; i < frames.length; i++) {
+            frames[i] = createFrame(i); // Создаем кадры анимации
+        }
+
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    }
+
+    private BufferedImage createFrame(int frameNumber) {
+        BufferedImage frame = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = frame.createGraphics();
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(0, 0, WIDTH, HEIGHT);
+        g2d.setColor(Color.BLUE);
+        g2d.setFont(new Font("Arial", Font.BOLD, 24));
+        g2d.drawString("Frame " + frameNumber, 100, 100);
+        g2d.dispose();
+        return frame;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (frames[currentFrame] != null) {
+            g.drawImage(frames[currentFrame], 0, 0, this);
+        }
+    }
+
+    @Override
+    public void run() {
+        long startTime;
+        long sleepTime;
+
+        while (running) {
+            startTime = System.currentTimeMillis();
+
+            // Обновляем текущий кадр
+            repaint();
+            currentFrame = (currentFrame + 1) % frames.length;
+
+            // Определяем время для ожидания
+            sleepTime = 1000 / FRAME_RATE - (System.currentTimeMillis() - startTime);
+
+            if (sleepTime > 0) {
+                try {
+                    Thread.sleep(sleepTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void startAnimation() {
+        if (animationThread == null) {
+            running = true;
+            animationThread = new Thread(this);
+            animationThread.start();
+        }
+    }
+
+    public void stopAnimation() {
+        running = false;
+        animationThread = null;
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Animation Example");
+            Task4 animation = new Task4();
+            frame.add(animation);
+            frame.pack();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+
+            // Запускаем анимацию
+            animation.startAnimation();
+        });
+    }
+}
