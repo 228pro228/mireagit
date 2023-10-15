1e729b0572f7d78f7150c3e632dc123343f9c1a5
Артём Чеботарёв <ponchikbom@gmail.com>
2023-10-15 22:40:19 +0300

    Lab7

---
+++ ru.mirea.lab7/src/MathCalculable.java
@@ -0,0 +1,5 @@
+interface MathCalculable {
+    double power(double base, double exponent);
+    double complexNumberModulus(double realPart, double imaginaryPart);
+    double getPi();
+}
---
+++ ru.mirea.lab7/src/StringOperations.java
@@ -0,0 +1,5 @@
+interface StringOperations {
+    int countCharacters(String s);
+    String oddPositionChars(String s);
+    String reverseString(String s);
+}
